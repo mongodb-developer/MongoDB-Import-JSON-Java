104a1fa2a5e7e87c68c0747d245bcca77a9bed3c
callbipin <callbipin911@gmail.com>
2021-06-30 04:12:56 -0700

    Update mongo.java

--- src/main/java/mongo.java
+++ src/main/java/mongo.java
@@ -1,32 +1,59 @@
-import com.mongodb.MongoWriteException;
-import com.mongodb.client.MongoClients;
+import java.io.*;
+import com.mongodb.bulk.BulkWriteResult;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.BulkWriteOptions;
+import com.mongodb.client.model.InsertOneModel;
+import com.mongodb.client.result.InsertManyResult;
 import com.mongodb.client.result.InsertOneResult;
-import org.apache.commons.io.IOUtils;
 import org.bson.Document;
+import com.mongodb.client.MongoClients;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import com.mongodb.MongoWriteException;
+import org.apache.commons.io.IOUtils;
+import org.bson.json.JsonObject;
+import javax.naming.StringRefAddr;
+ 
 public class mongo {
     public static void main(String[] args) throws IOException {
 
-        com.mongodb.client.MongoClient client = MongoClients.create( "mongodb+srv://<USERNAME>:<PASSWORD>@<CLUSTER NAME>/<DATABASE>?retryWrites=true&w=majority");
+        com.mongodb.client.MongoClient client = MongoClients.create( "<CONNECTION STRING>");
 
         MongoDatabase database = client.getDatabase("<DATABASE>");
-        MongoCollection<Document> coll = database.getCollection("<COLLECTION>");
+        MongoCollection<org.bson.Document> coll = database.getCollection("COLLECTION");
 
         try {
 
-            InputStream inStream = new FileInputStream("<FILEPATH>");
-            String body = IOUtils.toString(inStream, StandardCharsets.UTF_8.name());
-            Document myDoc = Document.parse(body);
+            //drop previous import
+            coll.drop();
 
-            InsertOneResult insertResult = coll.insertOne(myDoc);
-            System.out.println("Inserted" + insertResult);
+            //Bulk Approach:
+            int count = 0;
+            int batch = 100;
+            List<InsertOneModel<Document>> docs = new ArrayList<>();
+
+            try (BufferedReader br = new BufferedReader(new FileReader("<FILENAME>"))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    docs.add(new InsertOneModel<>(Document.parse(line)));
+                    count++;
+                    if (count == batch) {
+                        coll.bulkWrite(docs, new BulkWriteOptions().ordered(false));
+                        docs.clear();
+                        count = 0;
+                    }
+                }
+            }
+
+            if (count > 0) {
+                BulkWriteResult bulkWriteResult=  coll.bulkWrite(docs, new BulkWriteOptions().ordered(false));
+                System.out.println("Inserted" + bulkWriteResult);
+            }
 
         } catch (MongoWriteException e) {
             System.out.println("Error");
@@ -34,5 +61,3 @@ public class mongo {
 
     }
 }
-
-
